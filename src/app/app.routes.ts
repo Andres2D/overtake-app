1716b2a60986c3eedbdc89cbc562263c88bab2e3
Andres2D <andres2d1997.1206@gmail.com>
2024-04-10 14:55:09 -0400

    feat: add schedule component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ScheduleComponent } from './pages/schedule/schedule.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'schedule',
+    component: ScheduleComponent
+  },
+  {
+    path: '**',
+    pathMatch: 'full',
+    redirectTo: '/schedule'
+  }
+];
