d0d100edfabce992af0f90262bf093dbb1730743
Andres2D <andres2d1997.1206@gmail.com>
2024-04-12 12:27:29 -0400

    feat: implement meeting components in schedule page

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { MeetingComponent } from './components/meeting/meeting.component';
+import { meetingsMock } from '../../../mock/meeting.mock';
 
 @Component({
   selector: 'app-schedule',
   standalone: true,
-  imports: [],
+  imports: [
+    MeetingComponent,
+    NgFor
+  ],
   templateUrl: './schedule.component.html',
   styleUrl: './schedule.component.scss'
 })
-export class ScheduleComponent {}
+export class ScheduleComponent {
+  readonly meetingsMock = meetingsMock;
+
+  goToMeeting(meetingId: number): void {
+    console.log(meetingId);
+  }
+}
