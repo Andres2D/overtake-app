4e89c941814b3251f193f7d6b2b3b2837df57114
Andres2D <andres2d1997.1206@gmail.com>
2024-04-13 02:41:01 -0400

    refactor: add wrapper component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ScheduleComponent } from './pages/schedule/schedule.component';
+import { MeetingComponent } from './pages/meeting/meeting.component';
 
 export const routes: Routes = [
   {
     path: 'schedule',
     component: ScheduleComponent
+  },
+  {
+    path: 'meeting/:id',
+    component: MeetingComponent
   },
   {
     path: '**',
